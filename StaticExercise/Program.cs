76fd04e881108de3d7739f41f314bae34b7c749d
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-12-13 11:46:05 -0500

    completed exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a temperature in Fahrenheit that you would like to convert to Celcius:");
+            double temp = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{temp} degrees Fahrenheit is {TempConverter.FahrenheitToCelsius(temp)} degrees Celcius.");
+            Console.WriteLine();
+            Console.WriteLine("Please enter a temperature in Celcius that you would like to convert to Fahrenheit:");
+            double temp2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{temp2} degrees Celcius is {TempConverter.CelsiusToFahrenheit(temp2)} degrees Fahrenheit.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32) * 5 / 9;
+        }
+        public static double CelsiusToFahrenheit(double celcius)
+        {
+            return (celcius * 9) / 5 + 32;
+        }
+    }
+}
